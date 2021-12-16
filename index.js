e479a8aef3582c42fafbddd794aa3fabad99986c
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-12-16 15:36:20 -0300

    Cria actionCreator logIn, adiciona switch case ao "Reducer" possibilitando a mudanca de estado da aplicaçao, chama funcao logIn atraves da funçao dispatch

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const Redux = require('redux');
 
+// actionCreator, função que retorna uma action, tem pelo menos uma propriedade type
+const logIn = (email) => ({
+  type: 'LOGIN',
+  email,
+});
+
 const ESTADO_INICIAL = {
   login: false,
   email: '',
 };
 
-const reducer = (state = ESTADO_INICIAL) => {
-  return state;
+const reducer = (state = ESTADO_INICIAL, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        login: !state.login,
+        email: action.email,
+      };
+
+    default:
+      return state;
+  }
 };
 
 const store = Redux.createStore(reducer);
+
+store.dispatch(logIn('email@email.com'));
+
+console.log(store.getState());
